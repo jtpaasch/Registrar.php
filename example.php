41b49a6ac8699f31e1d487df160981d6601d14c7
JT Paasch <jpaasch@theatlantic.com>
2012-07-08 12:43:16 -0400

    Updated the example.php file, and made a small change to the Registrar.php file.

--- example.php
+++ example.php
@@ -1,82 +1,121 @@
 <?php
 	
-	// Make sure to load the Registrar.
-	include 'Registrar.php';
+// Make sure to load the Registrar.
+include 'Registrar.php';
 
-	// This class simply displays a message when it's instantiated.
-	class Message {
 
-		public function __construct() {
-			echo 'Hello World!';
-		}
+// ------------------------------------------------- //
+// 
+// Simple Usage
+// 
+// ------------------------------------------------- //
+
+// This class simply displays a message when it's instantiated.
+class Hello_World {
+
+	public function __construct() {
+		echo 'Hello World!' . '<br>';
 	}
+}
 
-	// Register the Message class with the Registrar.
-	Registrar::register(array(
+// Register the Hello_World class with the Registrar.
+Registrar::register(array(
 
-		// The key/token to reference the class with:
-		'key' => 'Message',
+	// The key/token to reference the class with:
+	'key' => 'Hello_World',
 
-		// The code to execute at checkout: 
-		'checkout' => function() {
-			return new Message();
-		},
+	// The code to execute at checkout: 
+	'checkout' => function() {
+		return new Hello_World();
+	},
 
-	));
+));
 
-	// Checkout an instance of the Message class.
-	$message = Registrar::checkout('Message');
+// Checkout an instance of the Hello_World class.
+$hello_world = Registrar::checkout('Hello_World'); 
+// Outputs: 	'Hello World!'
 
-	echo '<br>';
-
-	// This class simply displays a message when it's instantiated.
-	class Message {
-
-		public function __construct($message) {
-			echo $message;
-		}
-	}	
+echo '<hr>';
 
 
-	// class User {
+// ------------------------------------------------- //
+// 
+// Parameters 
+// 
+// ------------------------------------------------- //
 
-	// 	// Keep a reference to the name and email of the user.
-	// 	private $name;
-	// 	private $email;
+// This class displays the two messages passed to its constructor.
+class Message {
 
-	// 	// When a new user is created, store their name and email.
-	// 	public function __construct($name, $email) {
-	// 		$this->name = $name;
-	// 		$this->email = $email;		
-	// 	}
+	public function __construct($message1, $message2) {
+		echo $message1 . '<br>';
+		echo $message2 . '<br>';
+	}
+}	
 
-	// 	// Return the user's name and email. 
-	// 	public function __toString() {
-	// 		return $this->name . ' (' . $this->email . ')';
-	// 	}
+// Register the Message class with the Registrar.
+Registrar::register(array(
 
-	// }
+	// The key/token to reference the class with:
+	'key' => 'Message',
 
-	// // Create a user for John Smith, and a user for Sally Smith.
-	// $john = new User('John Smith', 'john@smith.com');
-	// $sally = new User('Sally Smith', 'sally@smith.com');
+	// The code to execute at checkout: 
+	'checkout' => function($message1, $message2) {
+		return new Message($message1, $message2);
+	},
 
-	// // List both users:
-	// echo $john . '<br>';
-	// echo $sally . '<br>';
+));
+
+// Checkout an instance of the Message class.
+$message = Registrar::checkout('Message', array('Hello!', 'Goodbye!')); 
+// Outputs: 	'Hello!'
+// 				'Goodbye!'
+
+echo '<hr>';
 
 
-	// // Register the User class with the Registrar
-	// Registrar::register(array(
+// ------------------------------------------------- //
+// 
+// Injections 
+// 
+// ------------------------------------------------- //
 
-	// 	// The key to reference the class with:
-	// 	'key' => 'User',
+// Register 'Version 1.0' and 'Copyright July 2012' with the Registrar's injections.
+// (Note: this is cumulative; the Registrar adds new injections to any already defined.)
+Registrar::injections(array('Version 1.0', 'Copyright July 2012'));
 
-	// 	// The code to execute at checkout: 
-	// 	'checkout' => function($name, $email) {
-	// 		return new User($name, $email);
-	// 	},
+// This class displays the two messages passed to its constructor, 
+// as well as a version number and a copyright date.
+class Messages_with_Status {
 
-	// ));
+	public function __construct($message1, $message2, $version, $copyright) {
+		echo $message1 . '<br>';
+		echo $message2 . '<br>';
+		echo $version . '<br>';
+		echo $copyright . '<br>';
+	}
+}	
+
+// Register the Messages_with_Status class with the Registrar.
+Registrar::register(array(
+
+	// The key/token to reference the class with:
+	'key' => 'Messages_with_Status',
+
+	// The code to execute at checkout: 
+	// Because there are two injections, we can name them and use them here: 
+	'checkout' => function($message1, $message2, $version, $copyright) {
+		return new Messages_with_Status($message1, $message2, $version, $copyright);
+	},
+
+));
+
+// Checkout an instance of the Messages_with_Status class.
+$messages = Registrar::checkout('Messages_with_Status', array('Hello!', 'Goodbye!')); 
+// Outputs: 	'Hello!'
+// 				'Goodbye!'
+// 				'Version 1.0'
+// 				'Copyright July 2012'
+
 
 ?>
